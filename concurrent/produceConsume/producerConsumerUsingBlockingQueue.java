e95df1e21aa2bce64a93f26376f0da1ce3009f5a
pkumar16 <Prashant.Kumar16@harman.com>
2020-12-28 21:25:57 +0530

    added producer consumer with blocking queue

---
+++ concurrent/produceConsume/producerConsumerUsingBlockingQueue.java
@@ -0,0 +1,47 @@
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ProducerConsumerPattern {
+
+    public static void main(String[] args) throws InterruptedException {
+        Queue<Integer> queue = new ArrayBlockingQueue<>(5);
+
+        Thread producer = new Thread(() -> {
+            
+                while(!Thread.currentThread().isInterrupted()) {
+                        Integer item = new Random().nextInt(10);
+                        if(queue.offer(item))
+                        System.out.println("Item produced : " + item + " queue size :" + queue.size());
+            }
+                System.out.println("Producer is Interrupted");
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                Integer item = queue.poll();
+                if(null != item)
+                System.out.println("Item consumed : " + item+ " queue size :" + queue.size());
+            }
+        });
+
+        producer.start();
+        consumer.start();
+        
+        Thread.sleep(2000);
+        
+        producer.interrupt();
+    }
+
+}
+
+class CustomBlockingQueue {
+
+}
+

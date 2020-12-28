bd8bef00ca8e4e7b1d6125596013b785d848a07d
pkumar16 <Prashant.Kumar16@harman.com>
2020-12-28 23:03:25 +0530

    file name updated

--- concurrent/produceConsume/producerConsumerUsingBlockingQueue.java
+++ concurrent/produceConsume/producerConsumerUsingBlockingQueue.java
@@ -23,7 +23,6 @@ public class ProducerConsumerPattern {
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
                 Integer item = queue.poll();
